455dfb0a728528f25ae3d1952bf749af86511445
Артур Филонов <filonov.artur@bk.ru>
2022-09-21 12:30:44 +0300

    3 task

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,16 @@
+const dead = document.getElementById('dead');
+const lost = document.getElementById('lost');
+let countDead = 0;
+let countLost = 0;
+getHole = index => document.getElementById(`hole${index}`);
+
+for (let i = 0; i < 10; i++) {
+
+hole.addEventListener('click', function() {
+if ( hole.classList.contains('hole_has-mole')) {
+countDead++;
+dead.textContent = countDead;
+} else {
+countLost++;
+lost.textContent = countLost;
+}
