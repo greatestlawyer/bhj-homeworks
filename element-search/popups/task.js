f6a3bea2ebfa701090c7c09d1e521a6eb075edf8
Артур Филонов <filonov.artur@bk.ru>
2022-09-28 11:21:56 +0300

    added 3 task again

---
+++ element-search/popups/task.js
@@ -0,0 +1,14 @@
+const modalClose = document.querySelector('.modal__close');
+const modal = document.querySelector('.modal');
+const showSuccess = document.querySelector('.show-success');
+const modalSuccess = document.getElementById('modal_success')
+
+
+modalClose.addEventListener ('click', function() {
+  modal.classList.remove('modal_active');
+  })
+
+showSuccess.addEventListener ('click', function() {
+  
+  })
+
--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,39 +1,31 @@
 //Задача 3
-const holeOne = document.getElementById('hole1');
-const holeTwo = document.getElementById('hole2');
-const holeThree = document.getElementById('hole3');
-const holeFour = document.getElementById('hole4');
-const holeFive = document.getElementById('hole5');
-const holeSix = document.getElementById('hole6');
-const holeSeven = document.getElementById('hole7');
-const holeEight = document.getElementById('hole8');
-const holeNine = document.getElementById('hole9');
-
+const holes = document.querySelectorAll('.hole');
 const dead = document.getElementById('dead');
 const lost = document.getElementById('lost');
 let countDead = 0;
 let countLost = 0;
 
-const arrayHoles = [];
-arrayHoles.push(holeOne, holeTwo, holeThree, holeFour, holeFive, holeSix, holeSeven, holeEight, holeNine);
-
-for (let i = 0; i < arrayHoles.length; i++) {
-  return arrayHoles[i].addEventListener('click', function() {
-    if (hole.classList.contains('hole_has-mole')) {
+for (let i = 0; i < holes.length; i++) {
+  holes[i].addEventListener('click', function() {
+    if (holes[i].classList.contains('hole_has-mole')) {
       countDead++;
       dead.textContent = countDead;
     } else {
         countLost++;
-	    lost.textContent = countLost;
+	      lost.textContent = countLost;
     }
-  }
-  if (countDead === '5'){
-    dead.textContent = countDead;
-    alert('Поражение!');
-    countDead = 0;
-  } else if (countLost === '10'){
+    if (countLost === 5){
       lost.textContent = countLost;
-      alert('Победа!');
+      alert('Поражение!');
       countLost = 0;
-  }
+      countDead = 0;
+  } else if (countDead === 10){
+      dead.textContent = countDead;
+      alert('Победа!');
+      countDead = 0;
+      countLost = 0;
+    }
+  });
 }
+
+
